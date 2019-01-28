2961baa2d33c7f6faca3ae3a30844f251f2ef9c0
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-28 09:22:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,7 @@ class Anagram
   
   def match(array)
     array.each do |x|
-      if x.split("").sort == @word return true
-    end
+      return x if x.split("").sort == @word 
     
     return []
     
